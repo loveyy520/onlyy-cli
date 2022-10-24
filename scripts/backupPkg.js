3d7079b7492e2e9dc70f20bcea9cf6e06213adc6
loveyy520 <201357337@qq.com>
2022-10-24 15:34:55 +0800

    'something-new'

--- scripts/backupPkg.js
+++ scripts/backupPkg.js
@@ -2,7 +2,7 @@
  * @Author: loveyy520 201357337@qq.com
  * @Date: 2022-10-24 12:55:39
  * @LastEditors: loveyy520 201357337@qq.com
- * @LastEditTime: 2022-10-24 15:04:05
+ * @LastEditTime: 2022-10-24 15:33:56
  * @FilePath: \onlyy-cli\scripts\publish.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -17,12 +17,22 @@ const devPkg = await readJson(pkg);
 
 const ignoredKeys = ["devDependencies", "scripts"];
 
+// 新版本
+const [mainVersion, preVersion] = devPkg.version.split("-dev-");
+const newVersion = String(parseInt(preVersion.replace(/\./g, "")) + 1)
+  .padStart(3, "0")
+  .split("")
+  .join(".");
+const version = `${mainVersion}-dev-${newVersion}`;
+
 const publishedPkg = Reflect.ownKeys(devPkg).reduce((pre, cur, i) => {
   if (!ignoredKeys.includes(cur)) {
     pre[cur] = devPkg[cur];
   }
   return pre;
 }, {});
+
+publishedPkg.version = version;
 
 const tempDir = resolve(cwd, "scripts/temp");
 const pkgName = "package.json";
--- scripts/resetDevPkg.js
+++ scripts/resetDevPkg.js
@@ -2,7 +2,7 @@
  * @Author: loveyy520 201357337@qq.com
  * @Date: 2022-10-24 13:42:49
  * @LastEditors: loveyy520 201357337@qq.com
- * @LastEditTime: 2022-10-24 15:05:15
+ * @LastEditTime: 2022-10-24 15:14:40
  * @FilePath: \onlyy-cli\scripts\resetDevPkg.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
