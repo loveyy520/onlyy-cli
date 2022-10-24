ca18f1d2d9174719343938ce687a4515b1f04cc2
loveyy520 <201357337@qq.com>
2022-10-24 20:26:54 +0800

    change: use self templates and more templates added.

--- helpers/download.js
+++ helpers/download.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Loveyy 201357337@qq.com
  * @Date: 2022-10-23 08:30:34
- * @LastEditors: Loveyy 201357337@qq.com
- * @LastEditTime: 2022-10-23 10:12:39
+ * @LastEditors: loveyy520 201357337@qq.com
+ * @LastEditTime: 2022-10-24 20:15:30
  * @FilePath: \onlyy-cli\helpers\download.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,7 +14,7 @@ import { callWithLoading } from "./loading.js";
 // downloadGitRepo 不支持 promise ，使用 node.js 的 util 为其提供 promise 能力
 export const download = async (repo, tag, targetDir) => {
   // 模板下载地址
-  const templateURL = `zhurong-cli/${repo}${tag ? "#" + tag : ""}`;
+  const templateURL = `loveyy520/${repo}${tag ? "#" + tag : ""}`;
   console.log(templateURL);
   const download = promisify(downloadGitRepo);
 
--- helpers/request.js
+++ helpers/request.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Loveyy 201357337@qq.com
  * @Date: 2022-10-23 00:25:50
- * @LastEditors: Loveyy 201357337@qq.com
- * @LastEditTime: 2022-10-23 01:48:03
+ * @LastEditors: loveyy520 201357337@qq.com
+ * @LastEditTime: 2022-10-24 20:25:20
  * @FilePath: \onlyy-cli\helpers\request.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,8 +11,8 @@ import axios from "axios";
 const timeout = 60000; // 60 s
 const baseURL = "https://api.github.com";
 
-// zhurong-cli 的 git 模版
-const repoURL = "/orgs/zhurong-cli/repos";
+const repoURL = "/repos/loveyy520/onlyy-templates/tags";
+const repoName = "onlyy-templates";
 
 const request = axios.create({
   timeout,
@@ -29,12 +29,13 @@ request.interceptors.response.use((res) => res.data);
  * 获取仓库信息
  * @returns Promise 仓库信息
  */
-export const getRepoList = async () => request.get(repoURL);
+// export const getRepoList = async () => request.get(repoURL);
+
+export const getRepo = () => repoName;
 
 /**
  * 获取版本信息
  * @param {string} repo 模板名称
  * @returns Promise 版本信息
  */
-export const getTagsByRepo = async (repo) =>
-  request.get(`/repos/zhurong-cli/${repo}/tags`);
+export const getTagsByRepo = async () => request.get(repoURL);
